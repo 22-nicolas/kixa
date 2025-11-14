7b103d4958f88cddd90d9ff1e14b67df5df7726c
Nicolas Tutuianu <nicolastutuianu04@gmail.com>
2025-11-14 17:41:07 +0100

    Add dots and goToSlide method to slider and match contents accordingly

--- react-frontend/src/elements/index/Slider.jsx
+++ react-frontend/src/elements/index/Slider.jsx
@@ -4,41 +4,86 @@ import hikingVid from "../../assets/hiking.mp4"
 import skateVid from "../../assets/skate.mp4"
 import runningImg from "../../assets/running.jpg"
 import { isVideo } from "../../modules/utils"
+import { useEffect, useRef, useState } from "react"
 
-const slidesData = [
-    {src: hikingVid, txt: "Long Way? Your Shoes Can Handle It."},
-    {src: skateVid, txt: "Built to Shred. Made to Last."},
-    {src: runningImg, txt: "Built for Speed. Born to Race."}
-]
 
 function Slider() {
     
-    const slides = initSlides(slidesData)
+    const slidesData = [
+        {src: hikingVid, txt: "Long Way? Your Shoes Can Handle It."},
+        {src: skateVid, txt: "Built to Shred. Made to Last."},
+        {src: runningImg, txt: "Built for Speed. Born to Race."}
+    ]
 
+    const [currentSlide, setCurrentSlide] = useState(0)
+    const slider = useRef()
+    const track = useRef()
+    const contents = useRef([])
+    const slides = initSlides()
+    let dots = initDots()
+    
+    useEffect(() => {
+        matchWidth()
+        window.addEventListener('resize', matchWidth);
+    }, [])
+    
+    useEffect(() => {
+        dots = initDots()
+    }, [currentSlide])
 
+    function initSlides() {
+        return slidesData.map((slideData, i) => {
+            slideData.isVideo = isVideo(slideData.src)
+
+            return(
+            <div className="slide" key={i}>
+                {slideData.isVideo ? 
+                    <video className="contents" ref={content => contents.current[i] = content} src={slideData.src} autoPlay muted loop/> :
+                    <img className="contents" ref={content => contents.current[i] = content} src={slideData.src}/>
+                }
+                <h1>{slideData.txt}<p>KX</p></h1>
+            </div>
+            )
+        })
+    }
+
+    function initDots(){
+        return slidesData.map((_, i) => {
+            let isActive = false
+            if (i === currentSlide) {
+                isActive = true
+            }
+
+            return <Dot key={i} className={isActive ? "active" : ""} onMouseDown={() => goToSlide(i)}/>
+        })
+    }
+
+    function goToSlide(i){
+        track.current.style.left = `-${i*slider.current.offsetWidth}px`;
+        setCurrentSlide(i)
+    }
+
+    function matchWidth() {
+        for (let i = 0; i < contents.current.length; i++) {
+            contents.current[i].style.width = slider.current.offsetWidth + 'px';
+        }
+    }
 
     return(
         <Container>
-            <div className="slider">
-                <div className="slider-track" id="sliderTrack">
+            <div ref={slider} className="slider">
+                <div ref={track} className="slider-track" id="sliderTrack">
                     {slides}
                 </div>
-                <div className="dots" id="dotsContainer"></div>
+                <div className="dots">
+                    {dots}
+                </div>
             </div>
         </Container>
     )
 }
 export default Slider
 
-function initSlides(slidesData) {
-    return slidesData.map((slideData, i) => {
-        slideData.isVideo = isVideo(slideData.src)
-
-        return(
-        <div className="slide" key={i}>
-            {slideData.isVideo ? <video className="contents" src={slideData.src} autoPlay muted loop/> : <img className="contents" src={slideData.src}/>}
-            <h1>{slideData.txt}<p>KX</p></h1>
-        </div>
-        )
-    })
+function Dot({ className, onMouseDown}) {
+    return <span className={className} onMouseDown={onMouseDown}></span>
 }
