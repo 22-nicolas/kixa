7d7f466ede98f9029a11f2f0ad97311fa32710e7
Nicolas Tutuianu <nicolastutuianu04@gmail.com>
2025-11-25 15:30:33 +0100

    Clean up Color.jsx and make color checks reapply on reload.

--- react-frontend/src/elements/search/Color.jsx
+++ react-frontend/src/elements/search/Color.jsx
@@ -1,31 +1,52 @@
-import { useEffect, useRef, forwardRef } from "react"
+import { useEffect, useRef, useState } from "react"
 import PropTypes from "prop-types"
-import { useNavigate } from "react-router-dom"
-import { Colors, string } from "../../modules/colors"
+import { useNavigate, useSearchParams } from "react-router-dom"
 
 export default function Color({ colors }) {
 
-    colors = Object.entries(colors)
-    const colorSelectors = useRef([])
-    const colorSelectorsComponents = colors.map(color => <ColorSelector key={color[1]} ref={el => colorSelectors.current[color[1]] = el} color={color[1]} onChange={applyColors}/>)
+    const [activeColors, setActiveColors] = useState([])
+    const [searchParams] = useSearchParams()
     const navigate = useNavigate()
+    
+    colors = Object.entries(colors)
+    const colorSelectorsComponents = colors.map(color => {
+        const colorId = color[1]
+        const colorName = color[0]
+        return <ColorSelector 
+                    key={colorId} 
+                    colorName={colorName} 
+                    activeColors={activeColors} 
+                    colorId={colorId} 
+                    onChange={(e) => applyColors(colorId, e.target.checked)}/>
+    })
 
-    function applyColors() {
-        let activeColors = ""
-        colorSelectors.current.forEach((selector) => {
-            if (selector.querySelector("input").checked) {
-                activeColors += selector.dataset.color + "a"
-            }
+    useEffect(() => {
+        reapplyColors()
+    }, [searchParams])
+
+    useEffect(() => {
+        //forward searchParams
+        searchParams.set("colors", activeColors.join("a")) 
+        navigate("/search?" + searchParams)
+    }, [activeColors])
+
+    function applyColors(colorId, checked) {
+        console.log(checked)
+        setActiveColors(prev =>{
+            if (checked) return [...prev, colorId] //add colorId
+            console.log({prev: prev, colorId: colorId})
+            return prev.filter(id => id !== String(colorId)) //remove colorId
         })
-
-        const params = new URLSearchParams(window.location.search)
-        params.set("colors", activeColors) 
-
-        navigate("/search?" + params)
     }
 
     function reapplyColors() {
+        let colors = searchParams.get("colors")
         
+        if (!colors) return
+
+        colors = colors.split("a")
+
+        setActiveColors(colors)
     }
 
     return(
@@ -41,20 +62,22 @@ Color.PropTypes = {
     colors: PropTypes.object.isRequired
 }
 
-const ColorSelector = forwardRef(function ColorSelector({ color, onChange }, ref) {
+function ColorSelector({ colorId, colorName, activeColors, onChange }) {
     const span = useRef()
     useEffect(() => {
         //color in span
         span.current.style.backgroundColor = span.current.parentElement.htmlFor;
     }, [])
 
+    const isActive = activeColors.includes(String(colorId))
+
     return(
-        <label ref={ref} data-color={color} htmlFor={string(color)} onChange={onChange}>
-            <input type="checkbox" id={string(color)}/>
+        <label htmlFor={colorName}>
+            <input type="checkbox" id={colorName} checked={isActive} onChange={onChange}/>
             <span ref={span}></span>
         </label>
     )
-})
+}
 ColorSelector.PropTypes = {
     color: PropTypes.string.isRequired,
     onChange: PropTypes.func.isRequired
