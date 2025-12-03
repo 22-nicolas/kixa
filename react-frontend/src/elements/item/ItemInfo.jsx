d56b0ac4ce37e3305c9dea52bc8b4e7f7f1bdb6d
Nicolas Tutuianu <nicolastutuianu04@gmail.com>
2025-12-03 20:12:20 +0100

    Add size selectors to item page

--- react-frontend/src/elements/item/ItemInfo.jsx
+++ react-frontend/src/elements/item/ItemInfo.jsx
@@ -1,11 +1,15 @@
 import { ItemDataContext } from "../../pages/Item"
-import { useContext } from "react"
+import { useContext, useState, createContext } from "react"
 import styles from "../../styles/item.module.css"
 import cart_icon from "../../assets/cart_icon.png"
 
 export default function ItemInfo() {
     const itemData = useContext(ItemDataContext)
+    const [activeSize, setActiveSize] = useState(null)
+
     if (!itemData) return <h1>Loading...</h1>
+
+    const sizeSelectors = itemData.sizes.map(size => <SizeSelector key={size} size={size} activeSize={activeSize} setActiveSize={setActiveSize}/>)
 
     return(
         <div className={styles.itemInfo}>
@@ -13,10 +17,11 @@ export default function ItemInfo() {
                 <h1>{itemData.name}</h1>
                 <p>{itemData.description}</p>  
                 <div className={styles.colorways}>
-                    
+
                 </div>
                 <p className={styles.sizeSelectLabel}>Select Size</p>
                 <div className={styles.sizeSelect}>
+                    {sizeSelectors}
                 </div>
                 <p className={styles.price}>{itemData.price}$</p>
                 <div className={styles.addCartButton}>
@@ -26,4 +31,10 @@ export default function ItemInfo() {
             </div>
         </div> 
     )
+}
+
+function SizeSelector({ size, activeSize, setActiveSize }) {
+    const isActive = activeSize === size
+
+    return <div className={`${styles.size} ${isActive ? styles.active : ""}`} onClick={() => setActiveSize(size)}>EU {size}</div>
 }
