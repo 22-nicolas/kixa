f68c437d88277bbbfad1fe209853be2949ad70cd
Nicolas Tutuianu <nicolastutuianu04@gmail.com>
2025-12-12 18:29:04 +0100

    Add asterix to required fields in register

--- react-frontend/src/elements/general/AccountPopup/Register.jsx
+++ react-frontend/src/elements/general/AccountPopup/Register.jsx
@@ -12,7 +12,7 @@ export default function Register() {
             <p>Please enter your details to create an account</p>
 
             {/*
-                Inputs will default to type: "text" and small: false.
+                Inputs will default to type: "text", small: false and required: true.
                 Id will be formated from label if not given.
 
                 TODO: Create phone number input with prefix dropdown.
@@ -22,7 +22,7 @@ export default function Register() {
             <RegisterDetails inputsData={[{label: "City"}, {label: "ZIP code", small: true}]}/>
             <RegisterDetails inputsData={[{label: "Street"}, {label: "House Number", small: true}]}/>
             <RegisterDetails inputsData={[{label: "Email", type: "email"}]}/>
-            <RegisterDetails inputsData={[{label: "Phone Number"}]}/>
+            <RegisterDetails inputsData={[{label: "Phone Number", required: false}]}/>
             <RegisterDetails inputsData={[{label: "Password", type: "password"}]}/>
             <RegisterDetails inputsData={[{label: "Repeat Password", type: "password"}]}/>
 
@@ -35,16 +35,17 @@ export default function Register() {
 function RegisterDetails({ inputsData }) {
 
     const inputComponents = inputsData.map((data, i) => {
-        let {label, id, small, type} = data
+        let {label, id, small, type, required} = data
 
         if (!id) id = label.toLocaleLowerCase().replaceAll(" ", "_")
         if (!type) type = "text"
+        if (required === undefined) required = true
 
         switch (type) {
             case "password":
                 return <Password key={i} label={label} id={id} small={small} i={i} />
             default:
-                return <Input key={i} label={label} type={type} id={id} small={small} i={i} />
+                return <Input key={i} label={label} type={type} id={id} small={small} i={i} required={required} />
         }
         
     })
@@ -56,10 +57,10 @@ function RegisterDetails({ inputsData }) {
     )
 }
 
-function Input({label, id, small, i, type }) {
+function Input({label, id, small, i, type, required }) {
     return(
         <div style={i == 2 ? {} : {marginRight: '4ch'}}>
-            <label htmlFor={id} className={styles.inputLabel}>{label}</label>
+            <label htmlFor={id} className={styles.inputLabel}>{label}{required ? " *" : ""}</label>
             <input id={id} type={type} className={`${styles.input} ${small ? styles.smallInput : ""}`} />
         </div>
     )  
@@ -74,7 +75,7 @@ function Password({label, id, small, i }) {
 
     return(
         <div style={i == 2 ? {} : {marginRight: '4ch'}}>
-            <label htmlFor={id} className={styles.inputLabel}>{label}</label>
+            <label htmlFor={id} className={styles.inputLabel}>{label} *</label>
             <div className={styles.password}>
                 <input id={id} type={`${isVisible ? "text" : "password"}`} className={`${styles.passwordInput} ${small ? styles.smallInput : ""}`} />
                 <img onMouseDown={toggleVisible} src={`${isVisible ? closedEye : openEye}`} alt="toggle visible" />
