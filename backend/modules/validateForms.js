4f478d80bec3c4e84aa1a437161eafa1fd0316cb
Nicolas Tutuianu <nicolastutuianu04@gmail.com>
2026-05-27 13:44:28 +0200

    Refactor users route to use module

---
+++ backend/modules/validateForms.js
@@ -0,0 +1,48 @@
+import { getUserByEmail } from "../sql/users.js";
+
+export const validateForms = {
+    login: ['email', 'password'],
+    register: [
+        'first_name', 'last_name', 'country', 'city', 'zip_code', 
+        'street', 'house_number', 'email', 'password', 
+        'repeat_password'
+    ],
+    address: [
+        'first_name', 'last_name', 'country', 'city', 'zip_code', 
+        'street', 'house_number', 'email'
+    ]
+}
+
+export async function validateForm(userData, requiredFields) {
+    if (!requiredFields) {
+        return { error: 'Invalid validation form' };
+    }
+
+    const missingFields = requiredFields.filter(field => !userData[field]);
+
+    if (missingFields.length > 0) {
+        return { error: `Missing required fields`, missing: missingFields };
+    }
+
+    // Validate email format using a regular expression
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(userData.email)) {
+        return { error: 'Invalid email format' };
+    }
+
+    // Return early for login validation.
+    if (requiredFields === validateForms.login) return null;
+
+    // Check if email is unique
+    const emailExists = await getUserByEmail(userData.email);
+    if (emailExists != null) {
+        return { error: 'Email already exists' };
+    }
+
+    // Check if repeat password matches password
+    if (userData.password !== userData.repeat_password) {
+        return { error: 'Passwords do not match' };
+    }    
+
+    return null; // Indicates successful validation
+}
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,22 +2,14 @@ import { getUserById, getUserByEmail, registerUser, createSession, validateSessi
 import { Router } from "express";
 import bcrypt from 'bcrypt';
 import crypto from "crypto";
+import { validateForms, validateForm } from "../modules/validateForms.js";
 
 const router = Router();
-
-const validateForms = {
-  login: ['email', 'password'],
-  register: [
-    'first_name', 'last_name', 'country', 'city', 'zip_code', 
-    'street', 'house_number', 'email', 'password', 
-    'repeat_password'
-  ]
-}
 
 router.post("/register", async (req, res) => {
   const userData = req.body;
   const {password} = userData;
-  const validateError = await validateUserData(userData, validateForms.register);
+  const validateError = await validateForm(userData, validateForms.register);
   if (validateError) {
     if (validateError === "Invalid validation form") {
       return res.status(400).send("Internal Server error: Invalid validation form");
@@ -37,7 +29,7 @@ router.post("/register", async (req, res) => {
 
 router.post("/login", async (req, res) => {
   const formValues = req.body;
-  const validateError = await validateUserData(formValues, validateForms.login);
+  const validateError = await validateForm(formValues, validateForms.login);
 
   if (validateError) {
     if (validateError === "Invalid validation form") {
@@ -93,37 +85,3 @@ router.get("/sessions/:id", async (req, res) => {
 });
 
 export default router;
-
-async function validateUserData(userData, requiredFields) {
-  if (!requiredFields) {
-    return { error: 'Invalid validation form' };
-  }
-
-  const missingFields = requiredFields.filter(field => !userData[field]);
-
-  if (missingFields.length > 0) {
-    return { error: `Missing required fields`, missing: missingFields };
-  }
-
-  // Validate email format using a regular expression
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!emailRegex.test(userData.email)) {
-    return { error: 'Invalid email format' };
-  }
-
-  // Return early for login validation.
-  if (requiredFields === validateForms.login) return null;
-
-  // Check if email is unique
-  const emailExists = await getUserByEmail(userData.email);
-  if (emailExists != null) {
-    return { error: 'Email already exists' };
-  }
-
-  // Check if repeat password matches password
-  if (userData.password !== userData.repeat_password) {
-    return { error: 'Passwords do not match' };
-  }    
-
-  return null; // Indicates successful validation
-}
